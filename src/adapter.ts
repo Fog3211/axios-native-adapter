57ffe496d4c3e1d2f22c8daf48b8471879648ce9
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2023-03-23 17:35:34 +0800

    fix: adapter header types

--- src/adapter.ts
+++ src/adapter.ts
@@ -8,32 +8,34 @@ export type InitAdapterOptions = {
 
 export const initAdapter = (
   options: InitAdapterOptions
-) => async (config: AxiosRequestConfig) => {
-  const { enable = isInApp } = options;
-  const isUseOriginalAdapter = !(typeof enable === 'function' ? enable(config) : enable);
-  const { method = 'GET', baseURL = '', url } = config;
+) => {
+  return (async (config: AxiosRequestConfig) => {
+    const { enable = isInApp } = options;
+    const isUseOriginalAdapter = !(typeof enable === 'function' ? enable(config) : enable);
+    const { method = 'GET', baseURL = '', url } = config;
 
-  if (!isUseOriginalAdapter) {
-    const nativeMethod = method.toUpperCase();
+    if (!isUseOriginalAdapter) {
+      const nativeMethod = method.toUpperCase();
 
-    return nativeRequest({
-      ...config,
-      url: `${baseURL}${url}`,
-      method: nativeMethod
-    }).then((res) => {
-      // 将bridgeResponse转换为Axios的响应格式
-      return {
-        data: res.data,
-        status: res.status,
-        statusText: res.statusText,
-        headers: config.headers,
-        config: config,
-        request: undefined,
-      };
-    });
-  } else {
-    // 调用默认的适配器处理需要删除自定义适配器，否则会死循环
-    delete config.adapter;
-    return axios(config);
-  }
+      return nativeRequest({
+        ...config,
+        url: `${baseURL}${url}`,
+        method: nativeMethod
+      }).then((res) => {
+        // 将bridgeResponse转换为Axios的响应格式
+        return {
+          data: res.data,
+          status: res.status,
+          statusText: res.statusText,
+          headers: config.headers,
+          config: config,
+          request: undefined,
+        };
+      });
+    } else {
+      // 调用默认的适配器处理需要删除自定义适配器，否则会死循环
+      delete config.adapter;
+      return axios(config);
+    }
+  }) as AxiosRequestConfig['adapter'];
 };
